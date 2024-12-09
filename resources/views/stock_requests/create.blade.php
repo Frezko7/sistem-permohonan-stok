836d76fdc5aaa7eabfc454d133e9343633f57eb8
Khairul Hafizi <khairulhafizie8@gmail.com>
2024-12-09 11:45:45 +0800

    commit

--- resources/views/stock_requests/create.blade.php
+++ resources/views/stock_requests/create.blade.php
@@ -47,6 +47,10 @@
                     value="{{ Auth::user()->phone_number }}" readonly>
             </div>
 
+            <!-- Hidden Stock Request ID -->
+            <input type="hidden" name="stock_request_id" id="stock_request_id"
+                value="{{ old('stock_request_id', $stockRequestId ?? '') }}">
+
             <!-- Stock Items Section (Dynamic) -->
             <div id="stock-items">
                 <div class="stock-item mb-4">
@@ -86,7 +90,6 @@
     <script>
         document.getElementById('add-stock-item').addEventListener('click', function() {
             let stockItemsContainer = document.getElementById('stock-items');
-            let stockRequestId = document.getElementById('stock_request_id').value;
 
             let stockItemHTML = `
                 <div class="stock-item mb-4">
@@ -94,15 +97,24 @@
                     <input type="text" class="input input-bordered w-full stock-id" name="stock_ids[]" placeholder="Masukkan kod stok" autocomplete="off" required>
                     
                     <ul class="stock-suggestions list-group position-absolute w-full bg-base-100 shadow-lg" style="z-index: 1000; display: none;"></ul>
-
+    
                     <label for="requested_quantity[]" class="label"><span class="label-text">Kuantiti Dimohon:</span></label>
                     <input type="number" class="input input-bordered w-full" name="requested_quantities[]" placeholder="Kuantiti stok" required>
-
-                    <!-- Include Stock Request ID -->
-                    <input type="hidden" name="stock_request_id[]" value="${stockRequestId}">
+    
+                    <button type="button" class="btn btn-danger mt-2 remove-stock-item">Buang Item</button>
                 </div>
             `;
+
+            // Append the new stock item to the container
             stockItemsContainer.insertAdjacentHTML('beforeend', stockItemHTML);
+
+            // Add event listener for removing the stock item
+            const removeButtons = stockItemsContainer.querySelectorAll('.remove-stock-item');
+            removeButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    button.closest('.stock-item').remove();
+                });
+            });
         });
     </script>
 @endsection
