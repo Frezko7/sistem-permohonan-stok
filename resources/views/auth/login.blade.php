0e80504489017644b6347f463db171b32780ad20
Khairul Hafizi <khairulhafizie8@gmail.com>
2025-01-02 15:17:00 +0800

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
 
                 <!-- Email Address -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
+                    <x-input-label for="email" :value="__('Email:')" />
                     <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
                         :value="old('email')" required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -28,7 +28,7 @@
 
                 <!-- Password -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
+                    <x-input-label for="password" :value="__('Kata Laluan:')" />
                     <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                         autocomplete="current-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -47,7 +47,7 @@
                 <div class="flex items-center justify-end mt-4">
 
                     <x-primary-button class="ms-3">
-                        {{ __('Log in') }}
+                        {{ __('Log Masuk') }}
                     </x-primary-button>
                 </div>
             </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
 
                 <!-- Name -->
                 <div>
-                    <x-input-label for="name" :value="__('Name')" />
+                    <x-input-label for="name" :value="__('Nama:')" />
                     <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
                         required autofocus autocomplete="name" />
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
@@ -33,7 +33,7 @@
 
                 <!-- Password -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
+                    <x-input-label for="password" :value="__('Kata Laluan:')" />
                     <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                         autocomplete="new-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -41,7 +41,7 @@
 
                 <!-- Confirm Password -->
                 <div class="mt-4">
-                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                    <x-input-label for="password_confirmation" :value="__('Sahkan  Kata Laluan:')" />
                     <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
                         name="password_confirmation" required autocomplete="new-password" />
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
@@ -79,7 +79,7 @@
 
                 <!-- Phone Number -->
                 <div class="mt-4">
-                    <x-input-label for="phone_number" :value="__('Phone Number')" />
+                    <x-input-label for="phone_number" :value="__('No. Telefon:')" />
                     <x-text-input id="phone_number" class="block mt-1 w-full" type="tel" name="phone_number"
                         :value="old('phone_number')" required />
                     <x-input-error :messages="$errors->get('phone_number')" class="mt-2" />
@@ -88,7 +88,7 @@
                 <div class="flex items-center justify-end mt-4">
 
                     <x-primary-button class="ms-4">
-                        {{ __('Register') }}
+                        {{ __('Daftar') }}
                     </x-primary-button>
                 </div>
             </form>
